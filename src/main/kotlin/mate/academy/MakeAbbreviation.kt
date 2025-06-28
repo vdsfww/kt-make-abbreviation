820960aec05fc2633d61fbaf2338e3665d3488ca
vdsfww <loppprt@gmail.com>
2025-06-28 23:42:18 +0300

    hw

--- src/main/kotlin/mate/academy/MakeAbbreviation.kt
+++ src/main/kotlin/mate/academy/MakeAbbreviation.kt
@@ -1,6 +1,10 @@
 package mate.academy
 
-fun makeAbbr(source: String) : String {
-    // implement this function
-    return ""
+fun makeAbbr(source: String): String {
+    return source
+        .split(" ")
+        .filter { it.isNotEmpty() }
+        .map { it[0] }
+        .joinToString("")
+        .uppercase()
 }
